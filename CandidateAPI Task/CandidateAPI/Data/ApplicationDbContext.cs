5b828278ef527d147e9de3d941472dcb3ebf02db
SUMAN <nick.suman77@gmail.com>
2024-11-06 23:46:10 +0545

    Initial Project SetUp

---
+++ CandidateAPI Task/CandidateAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CandidateAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Enforce unique constraint on Email field
+            builder.Entity<Candidate>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+        }
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
---
+++ CandidateAPI Task/CandidateAPI/Models/Candidate.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandidateAPI.Models
+{
+    public class Candidate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string PreferredCallTime { get; set; }
+
+        [Url]
+        public string LinkedInProfile { get; set; }
+
+        [Url]
+        public string GitHubProfile { get; set; }
+
+        [Required]
+        public string Comments { get; set; }
+    }
+}
